68f17d6129aad26668547aafa98722c9b6c2c69c
danya <danyapancresnicov@gmail.com>
2023-06-02 14:20:35 +0300

    JS галочики

--- js/home.js
+++ js/home.js
@@ -1,69 +1,92 @@
 function updateInputFields() {
-    var numFieldsInput = document.getElementById('numFields');
-    var numFields = parseInt(document.getElementById('numFields').value);
+  var numFieldsInput = document.getElementById('numFields');
+  var numFields = parseInt(document.getElementById('numFields').value);
 
-    // Ограничение допустимого значения
-    var min = 2;
-    var max = 10;
-    if (numFields < min) {
-        numFields = min;
-    } else if (numFields > max) {
-        numFields = max;
+  // Ограничение допустимого значения
+  var min = 2;
+  var max = 10;
+  if (numFields < min) {
+    numFields = min;
+  } else if (numFields > max) {
+    numFields = max;
+  }
+  numFieldsInput.value = numFields;
+
+  var gridContainer1 = document.getElementById('gridContainer1');
+  var gridContainer2 = document.getElementById('gridContainer2');
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  var storedCheckboxStates = {}; // Stores the checkbox states from localStorage
+
+  checkboxes.forEach(function (checkbox) {
+    var checkboxState = localStorage.getItem(checkbox.id);
+    if (checkboxState !== null) {
+      storedCheckboxStates[checkbox.id] = checkboxState === 'true';
     }
-    numFieldsInput.value = numFields;
+  });
 
-    var gridContainer1 = document.getElementById('gridContainer1');
-    var gridContainer2 = document.getElementById('gridContainer2');
-    gridContainer1.innerHTML = ''; // Clear existing grid 1
-    gridContainer2.innerHTML = ''; // Clear existing grid 2
+  gridContainer1.innerHTML = ''; // Clear existing grid 1
+  gridContainer2.innerHTML = ''; // Clear existing grid 2
 
-    for (var i = 0; i < numFields; i++) {
-        var gridRow1 = document.createElement('div');
-        var gridRow2 = document.createElement('div');
-        gridRow1.className = 'grid-row';
-        gridRow2.className = 'grid-row';
+  for (var i = 0; i < numFields; i++) {
+    var gridRow1 = document.createElement('div');
+    var gridRow2 = document.createElement('div');
+    gridRow1.className = 'grid-row';
+    gridRow2.className = 'grid-row';
 
-        for (var j = 0; j < numFields; j++) {
-            var gridItem1 = document.createElement('div');
-            var gridItem2 = document.createElement('div');
-            gridItem1.className = 'grid-item';
-            gridItem2.className = 'grid-item';
-            var checkbox1 = document.createElement('input');
-            var checkbox2 = document.createElement('input');
-            checkbox1.type = 'checkbox';
-            checkbox2.type = 'checkbox';
+    for (var j = 0; j < numFields; j++) {
+      var gridItem1 = document.createElement('div');
+      var gridItem2 = document.createElement('div');
+      gridItem1.className = 'grid-item';
+      gridItem2.className = 'grid-item';
+      var checkbox1 = document.createElement('input');
+      var checkbox2 = document.createElement('input');
+      checkbox1.type = 'checkbox';
+      checkbox2.type = 'checkbox';
 
-            // Disable checkboxes where i < j or i = j
-            if (i < j || i === j) {
-                checkbox1.disabled = true;
-                checkbox2.disabled = true;
-            }
+      // Disable checkboxes where i < j or i = j
+      if (i < j || i === j) {
+        checkbox1.disabled = true;
+        checkbox2.disabled = true;
+      }
 
-            // Assign checkbox IDs
-            checkbox1.id = i + " " + j;
-            checkbox2.id = i + " " + j + "grid2"; // Add "_2" to the ID of checkboxes in the second grid
+      // Assign checkbox IDs
+      checkbox1.id = i + ' ' + j;
+      checkbox2.id = i + ' ' + j + ' sec'; // Add "_2" to the ID of checkboxes in the second grid
 
-            gridItem1.appendChild(checkbox1);
-            gridRow1.appendChild(gridItem1);
-            gridItem2.appendChild(checkbox2);
-            gridRow2.appendChild(gridItem2);
-        }
+      gridItem1.appendChild(checkbox1);
+      gridRow1.appendChild(gridItem1);
+      gridItem2.appendChild(checkbox2);
+      gridRow2.appendChild(gridItem2);
 
-        gridContainer1.appendChild(gridRow1);
-        gridContainer2.appendChild(gridRow2);
+      // Set the checked state from stored values
+      if (storedCheckboxStates[checkbox1.id] !== undefined) {
+        checkbox1.checked = storedCheckboxStates[checkbox1.id];
+      }
+
+      // Call checkRelatedCheckboxes if checkbox is checked
+      if (checkbox1.checked) {
+        checkRelatedCheckboxes(checkbox1);
+      }
     }
 
-    // Add event listeners to checkboxes
-    addCheckboxEventListeners();
-    localStorage.setItem('numFields', numFields);
+    gridContainer1.appendChild(gridRow1);
+    gridContainer2.appendChild(gridRow2);
+  }
+
+  // Add event listeners to checkboxes
+  addCheckboxEventListeners();
+  localStorage.setItem('numFields', numFields);
 }
+
+
+
 
 function addCheckboxEventListeners() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
     checkboxes.forEach(function(checkbox) {
         checkbox.addEventListener('change', function() {
             checkRelatedCheckboxes(checkbox);
-            var gridContainerIndex = checkbox.id.endsWith("_2") ? 'gridContainer2' : 'gridContainer1';
+            var gridContainerIndex = checkbox.id.endsWith("grid2") ? 'gridContainer2' : 'gridContainer1';
             // Save the checkbox state to localStorage with grid container index and checkbox id as the key
             localStorage.setItem(gridContainerIndex + ':' + checkbox.id, checkbox.checked);
         });
@@ -71,40 +94,48 @@ function addCheckboxEventListeners() {
 }
 
 function loadSavedData() {
-    // Load the previously entered number of fields from localStorage
-    var numFields = localStorage.getItem('numFields');
-    if (numFields) {
-        document.getElementById('numFields').value = numFields;
-        updateInputFields();
-    }
-    // Load the checkbox states from localStorage
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(function(checkbox) {
-        // Get the grid container index
-        var gridContainerIndex = checkbox.id.endsWith("_2") ? 'gridContainer2' : 'gridContainer1';
+  // Load the previously entered number of fields from localStorage
+  var numFields = localStorage.getItem('numFields');
+  if (numFields) {
+    document.getElementById('numFields').value = numFields;
+    updateInputFields();
+  }
 
-        // Get the checkbox state from localStorage using grid container index and checkbox id as the key
-        var checkboxState = localStorage.getItem(gridContainerIndex + ':' + checkbox.id);
-        if (checkboxState) {
-            checkbox.checked = checkboxState === 'true';
-        }
-    });
+  // Load the checkbox states from localStorage
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(function(checkbox) {
+    // Get the grid container index
+    var gridContainerIndex = checkbox.id.endsWith("_2") ? 'gridContainer1' : 'gridContainer2';
+
+    // Get the checkbox state from localStorage using grid container index and checkbox id as the key
+    var checkboxState = localStorage.getItem(checkbox.id);
+    if (checkboxState) {
+      checkbox.checked = checkboxState === 'true';
+    }
+  });
 }
 
 function checkRelatedCheckboxes(checkbox) {
-    var checkboxId = checkbox.id.split(" ");
-    var mirroredId = checkboxId[1] + " " + checkboxId[0];
-
-    var isCheckboxInSecondGrid = checkbox.id.endsWith("_2");
-    var mirroredCheckboxId = isCheckboxInSecondGrid ? mirroredId + "_2" : mirroredId;
-
-    var mirroredCheckbox = document.getElementById(mirroredCheckboxId);
-
-    if (mirroredCheckbox) {
-        mirroredCheckbox.checked = checkbox.checked;
-    }
+  var checkboxId = checkbox.id.split(" ");
+  if (checkboxId[2] != "sec") {
+    var isCheckboxInSecondGrid = true;
+  } else {
+    var isCheckboxInSecondGrid = false;
+  }
+  var mirroredId = checkboxId[1] + " " + checkboxId[0];
+  var mirroredIdSec = checkboxId[1] + " " + checkboxId[0] + " sec";
+  var mirroredCheckbox = document.getElementById(mirroredId);
+  var mirroredCheckboxSec = document.getElementById(mirroredIdSec);
+  if (isCheckboxInSecondGrid) {
+    mirroredCheckbox.checked = checkbox.checked;
+    // Save checkbox state to localStorage
+    localStorage.setItem(checkbox.id, checkbox.checked);
+  } else {
+    mirroredCheckboxSec.checked = checkbox.checked;
+    // Save checkbox state to localStorage
+    localStorage.setItem(checkbox.id, checkbox.checked);
+  }
 }
-
 
 function saveMatrices(url) {
     var gridContainer1 = document.getElementById('gridContainer1');
